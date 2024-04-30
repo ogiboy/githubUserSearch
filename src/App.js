bb488262cc33187fb030bba4320fcdd4ec417345
Oğuzcan Toptaş <oguzcantoptas@gmail.com>
2024-04-30 19:16:29 +0300

    update packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import SearchByName from "./components/SearchByName";
-import "./styles.css";
-import Link from "next/link";
+import SearchByName from './components/SearchByName'
+import './styles.css'
 
 export default function App() {
   return (
@@ -8,5 +7,5 @@ export default function App() {
       <h1>GitHub User Search</h1>
       <SearchByName />
     </div>
-  );
+  )
 }
